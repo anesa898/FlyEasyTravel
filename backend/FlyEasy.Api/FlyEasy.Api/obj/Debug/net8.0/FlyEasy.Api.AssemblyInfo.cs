33bd21ea23c4db04af55f09f8f018d8446bbb4f6
Anesa Haliti <anesa.haliti@example.com>
2026-06-15 04:12:13 +0200

    Dockerfile

--- backend/FlyEasy.Api/FlyEasy.Api/obj/Debug/net8.0/FlyEasy.Api.AssemblyInfo.cs
+++ backend/FlyEasy.Api/FlyEasy.Api/obj/Debug/net8.0/FlyEasy.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlyEasy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7efdbf23569996090bcc9dfdf589ff5aac7d1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlyEasy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlyEasy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
